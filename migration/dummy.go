072a1979fc5174d70992171459dced6399ba62f6
oleg578 <oleg.nagornij@gmail.com>
2024-05-28 19:11:30 +0200

    Remove CSV generation features from dummy migration

--- migration/dummy.go
+++ migration/dummy.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"csvdump/employee"
-	"encoding/csv"
 	"flag"
 	"fmt"
 	"github.com/go-faker/faker/v4"
-	"math/rand"
 	"os"
 	"strings"
-	"time"
 )
 
 func main() {
@@ -17,30 +14,13 @@ func main() {
 	dsn := flag.String("dsn", defaultDSN, "database connection string")
 	flag.Parse()
 
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	file, err := os.Create("dummy_data.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer func(file *os.File) {
-		_ = file.Close()
-	}(file)
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
 	for i := 1; i <= 1000000; i++ {
 		emp := randomEmp()
 		err := emp.Save(*dsn)
 		if err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "%v\n", err.Error())
-			//continue
 		}
-		//_ = writer.Write(emp.ToSlice())
 	}
-
-	fmt.Println("CSV file successfully generated.")
 }
 
 func randomEmp() employee.Employee {
